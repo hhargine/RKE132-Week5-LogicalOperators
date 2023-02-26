9b8c8df9a023197d733464124a39a4430041de79
Helina H <hhargine@tktk.ee>
2023-02-26 19:37:36 +0200

    adding week 5

---
+++ ANDOperator/Program.cs
@@ -0,0 +1,23 @@
+﻿//Math >= 90; Bilogy >=90, Chemistry >=90;
+
+int math, biology, chemistry;
+
+Console.WriteLine("Enter your Math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Bilogy result:");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemistry result:");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if(math >= 90 && biology >= 90 && chemistry >= 90)
+{
+    Console.WriteLine("Congratulations! You are accepted.");
+
+}
+else
+{
+    Console.WriteLine("Your application cannot be approved.");
+}
+
